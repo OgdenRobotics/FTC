5632ca25b951bdd73062f71dcaed85d479a4ad8b
Ogden International School Robotics Teams <34726909+OgdenRobotics@users.noreply.github.com>
2018-01-03 17:55:42 -0600

    Rev Op Mode Test
    This Android Studio Op Mode code successfully tested: a motor, servo, color sensor, touch sensor and gamepad on the Rev expansion hub.

--- Java/MyFIRSTJavaOpMode.java
+++ Java/MyFIRSTJavaOpMode.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 // This was copied from section 10.2.7 of: https://www.firstinspires.org/sites/default/files/uploads/resource_library/ftc/android-studio-tutorial.pdf
 
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -11,7 +12,10 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Gyroscope;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 @TeleOp
+//@Disabled
 public class MyFIRSTJavaOpMode extends LinearOpMode {
     private Gyroscope imu;
     private DcMotor motorTest;
@@ -25,6 +29,10 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
         sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
         servoTest = hardwareMap.get(Servo.class, "servoTest");
+
+        // set digital channel to input mode.
+        digitalTouch.setMode(DigitalChannel.Mode.INPUT);
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
         // Wait for the game to start (driver presses PLAY)
@@ -34,8 +42,30 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         while (opModeIsActive()) {
             tgtPower = -this.gamepad1.left_stick_y;
             motorTest.setPower(tgtPower);
+
+            // check to see if we need to move the servo.
+            if(gamepad1.y) {
+                // move to 0 degrees.
+                servoTest.setPosition(0);
+            } else if (gamepad1.x || gamepad1.b) {
+                // move to 90 degrees.
+                servoTest.setPosition(0.5);
+            } else if (gamepad1.a) {
+                // move to 180 degrees.
+                servoTest.setPosition(1);
+            }
+            // is button pressed?
+            if (digitalTouch.getState() == false) {
+                // button is pressed.
+                telemetry.addData("Button", "PRESSED");
+            } else {
+                // button is not pressed.
+                telemetry.addData("Button", "NOT PRESSED");
+            }
+            telemetry.addData("Servo Position", servoTest.getPosition());
             telemetry.addData("Target Power", tgtPower);
             telemetry.addData("Motor Power", motorTest.getPower());
+            telemetry.addData("Distance (cm)", sensorColorRange.getDistance(DistanceUnit.CM));
             telemetry.addData("Status", "Running");
             telemetry.update();
         }
